8442f9e995dc1de1bf2bfae2e9a5b9d46b9ab644
Jignesh <jigneshkushwaha63@gmail.com>
2024-07-31 16:03:36 +0530

    Assignment3-4

---
+++ Assignment4/ans1/Second.java
@@ -0,0 +1,13 @@
+package Assignment4.ans1;
+
+public class Second {
+    private int second;
+
+    public void setSecond(int second){
+        this.second = second;
+    }
+    
+    public int getSecond(){
+        return second;
+    }    
+}
